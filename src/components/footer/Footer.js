e96920a2f858123cb6d33c88a64cbc82c2e26599
Emmanuel_Sewah <sewah2012@gmail.com>
2022-10-20 17:47:44 +0000

    update

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,8 +3,10 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='footer'>
-      <h4>Designed and Developed by: <span>@BabySkinner</span></h4>
+    <div className="footer">
+      <h4>
+        Designed and Developed by: <span> Chinwe Emele </span>
+      </h4>
     </div>
   )
 }
